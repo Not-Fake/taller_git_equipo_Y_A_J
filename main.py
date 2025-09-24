5fb88b1bf14337160008109e7e7a4b1a10b4f831
A,j,y <cema0012@gmail.com>
2025-09-24 09:48:35 -0500

    potencia

---
+++ main.py
@@ -0,0 +1,6 @@
+def potencia():
+    nummero_1=float(input("numero que desea elevar"))
+    nummero_2=float(input("exponente"))
+    resultado=pow(nummero_1,nummero_2)
+    print(resultado)
+potencia()
