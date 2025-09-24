bb0e9a32644ee58e3464b58c7849ffd2d3a4c9db
Yaider <yaiderariza423@gmail.com>
2025-09-24 11:56:42 -0500

    rama principal, suma y resta

---
+++ mainVerdadero.py
@@ -0,0 +1,14 @@
+# =======================================
+# Equipo: Yaider martinez, Alan Andrés Orozco Sánchez, Juan Esteban pacheco
+# Proyecto: Taller de Git y Python
+# =======================================
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Suma:", suma(10, 5))
+    print("Resta:", resta(10, 5))
