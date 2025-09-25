358fb06700affe2da3ab76c9fd236b9caa46cfae
Not-Fake <yaidermartinez007@gmail.com>
2025-09-25 16:22:26 -0500

    funcion de potencia

--- main.py
+++ main.py
@@ -1,6 +1,2 @@
-def potencia():
-    nummero_1=float(input("numero que desea elevar"))
-    nummero_2=float(input("exponente"))
-    resultado=pow(nummero_1,nummero_2)
-    print(resultado)
-potencia()
+def potencia(a, b):
+    return a ** b
