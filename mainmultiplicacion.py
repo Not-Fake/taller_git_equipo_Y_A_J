2794d14dad11ecdc8453d16c17d0f0439b2b0f26
Juan Pacheco <juanesteban2006ph@gmail.com>
2025-09-25 15:44:36 -0500

    Agregada función multiplicar en mainmultiplicacion.py

---
+++ mainmultiplicacion.py
@@ -0,0 +1,17 @@
+# Equipo Y_A_J - Proyecto operaciones matemáticas
+
+def suma(a, b):
+    return a + b
+
+def resta(a, b):
+    return a - b
+
+
+def multiplicar(a, b):
+    return a * b
+
+if __name__ == "__main__":
+    print("Pruebas de funciones:")
+    print("Suma: ", suma(2, 3))
+    print("Resta: ", resta(5, 2))
+    print("Multiplicación: ", multiplicar(4, 6))  # tu función
